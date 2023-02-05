ef970f4f707737023244907b6a50a9c4a7853832
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-02-05 18:00:43 -0300

    Corrige import funcionario

--- Módulo 01/Semana 05/M1S05E1/Funcionario.js
+++ Módulo 01/Semana 05/M1S05E1/Funcionario.js
@@ -16,12 +16,3 @@ this.salario += (this.salario * percentual/100);
   }
 }
 
-
-const ada = new Funcionario("528.442.040-31", "Ada Lovelace", 1000);
-
-console.log(ada.salario); // 1000
-
-ada.promover(50);
-
-
-console.log(ada.salario); // 1500  
